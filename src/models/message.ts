23244f5b9662ec965c8629dfb974c80435df39d4
Robert Chen <bergintonatcapital@gmail.com>
2024-02-11 22:51:38 -0500

    Redo date fix

--- src/models/message.ts
+++ src/models/message.ts
@@ -18,10 +18,11 @@ const MessageSchema = new Schema({
 MessageSchema.virtual("format_date").get(function (this: IMessage) {
 	// Convert the stored UTC date to the user's local time
 	const localDate = DateTime.fromJSDate(this.post_date).toLocal();
-	// Format the local date and time
-	return localDate.toLocaleString(DateTime.DATETIME_MED);
-	
-	// return DateTime.fromJSDate(this.post_date).toLocaleString(DateTime.DATETIME_MED);
+	// Retrieve the timezone
+	const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+	// Format the local date and time along with the timezone
+	const formattedDate = `${localDate.toLocaleString(DateTime.DATETIME_MED)} ${timeZone}`;
+	return formattedDate;
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,30 +1,30 @@
-import { Document, Schema, model } from 'mongoose';
+import { Document, Schema, model } from "mongoose";
 import { DateTime } from "luxon";
 
 interface IUser {
-    username: string;
-    password: string;
-    member_status: string;
-    profile_pic: Number,
-    date_joined: Date;
+	username: string;
+	password: string;
+	member_status: string;
+	profile_pic: Number;
+	date_joined: Date;
 }
 
 export interface IUserDocument extends IUser, Document {
-    // Define additional instance methods or properties here
-    url: string;
+	// Define additional instance methods or properties here
+	url: string;
 }
 
 const UserSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    member_status: { type: String, required: true },
-    profile_pic: {type: Number, default: 0, required: true},
-    date_joined: { type: Date, default: Date.now, required: true },
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+	member_status: { type: String, required: true },
+	profile_pic: { type: Number, default: 0, required: true },
+	date_joined: { type: Date, default: Date.now, required: true },
 });
 
 UserSchema.virtual("format_date").get(function (this: IUserDocument) {
-	return `${DateTime.fromJSDate(this.date_joined).toLocaleString(DateTime.DATETIME_MED)} (Eastern Standard Time)`;
+	const easternDateTime = DateTime.fromJSDate(this.date_joined).setZone("America/New_York");
+	return `${easternDateTime.toLocaleString(DateTime.DATETIME_MED)} (Eastern Standard Time)`;
 });
 
 module.exports = model("User", UserSchema);
-
